81f99b4880b540061520974b9958b6874113ebc2
harukibun <hb@iMac.local>
2020-08-21 08:03:48 +0900

    first

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import Dropzone from 'react-dropzone';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import Dropzone from 'react-dropzone'
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       isUploading: false,
-      images: []
-    };
-    this.handleOnDrop = this.handleOnDrop.bind(this);
+      images: [],
+    }
+    this.handleOnDrop = this.handleOnDrop.bind(this)
   }
 
   handleOnDrop(files) {
-    this.setState({isUploading: true});
+    this.setState({ isUploading: true })
 
-    Promise.all(files.map(file => this.uploadImage(file)))
-      .then(images => {
+    Promise.all(files.map((file) => this.uploadImage(file)))
+      .then((images) => {
         this.setState({
           isUploading: false,
-          images: this.state.images.concat(images)
-        });
-      }).catch(e => console.log(e));
+          images: this.state.images.concat(images),
+        })
+      })
+      .catch((e) => console.log(e))
   }
 
   uploadImage(file) {
-    return axios.get('/upload', {
-      params: {
-        filename: file.name,
-        filetype: file.type
-      }
-    }).then(res => {
-      const options = {
-        headers: {
-          'Content-Type': file.type
+    return axios
+      .get('/upload', {
+        params: {
+          filename: file.name,
+          filetype: file.type,
+        },
+      })
+      .then((res) => {
+        const options = {
+          headers: {
+            'Content-Type': file.type,
+          },
         }
-      };
-      return axios.put(res.data.url, file, options);
-    }).then(res => {
-      const {name} = res.config.data;
-      return {
-        name,
-        isUploading: true,
-        url: `https://akameco-images.s3.amazonaws.com/${file.name}`
-      };
-    });
+        return axios.put(res.data.url, file, options)
+      })
+      .then((res) => {
+        const { name } = res.config.data
+        return {
+          name,
+          isUploading: true,
+          url: process.env.S3_URL + file.name,
+        }
+      })
   }
 
   render() {
@@ -58,20 +62,20 @@ class App extends Component {
       borderWidth: 2,
       borderColor: '#666',
       borderStyle: 'solid',
-      borderRadius: 5
-    };
+      borderRadius: 5,
+    }
 
     const activeStyle = {
       opacity: 0.5,
-      backgroundColor: '#eee'
-    };
+      backgroundColor: '#eee',
+    }
 
     const rejectStyle = {
-      backgroundColor: '#ffdddd'
-    };
+      backgroundColor: '#ffdddd',
+    }
 
     return (
-      <div style={{width: 760, margin: '30px auto'}}>
+      <div style={{ width: 760, margin: '30px auto' }}>
         <h1>React S3 Image Uploader Sample</h1>
         <Dropzone
           onDrop={this.handleOnDrop}
@@ -79,22 +83,23 @@ class App extends Component {
           style={divStyle}
           activeStyle={activeStyle}
           rejectStyle={rejectStyle}
-          >
-        {this.state.isUploading ?
-          <div>ファイルをアップロードしています</div> :
-          <div>ここに画像をドラックまたはクリック</div>}
+        >
+          {this.state.isUploading ? (
+            <div>ファイルをアップロードしています</div>
+          ) : (
+            <div>ここに画像をドラックまたはクリック</div>
+          )}
         </Dropzone>
-        {this.state.images.length > 0 &&
-          <div style={{margin: 30}}>
-            {this.state.images.map(({name, url}) =>
-              <img key={name} src={url} style={{width: 200, height: 200}}/>)}
-          </div>}
+        {this.state.images.length > 0 && (
+          <div style={{ margin: 30 }}>
+            {this.state.images.map(({ name, url }) => (
+              <img key={name} src={url} style={{ width: 200, height: 200 }} />
+            ))}
+          </div>
+        )}
       </div>
-    );
+    )
   }
 }
 
-render(
-  <App/>,
-  document.querySelector('#main')
-);
+render(<App />, document.querySelector('#main'))
